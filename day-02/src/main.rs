ebe3517fea987c567446592f0a7c7c5d0a42d354
n0rrman <mail@henriknorrman.com>
2024-12-03 01:02:24 +0100

    day 02 completed

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -7,7 +7,18 @@ fn reactor_conditions(list: &Vec<i32>) -> bool {
     return not_empty && max_distance && (strict_ascending || strict_descending);
 }
 
-fn a() -> i32 {
+fn relaxed_conditions(list: &Vec<i32>) -> bool {
+    for i in 0..list.len() {
+        let mut new_list = list.clone();
+        new_list.remove(i);
+        if reactor_conditions(&new_list) {
+            return true
+        }
+    }
+    return false
+}
+
+fn scan_reports(conditions: fn(&Vec<i32>) -> bool) -> i32 {
     let input_string = String::from_utf8_lossy(include_bytes!("data.txt"));
     let count = input_string
         .lines()
@@ -15,14 +26,18 @@ fn a() -> i32 {
             .filter_map(|y| y.parse::<i32>().ok())
             .collect::<Vec<i32>>()
         )
-        .filter(|x| reactor_conditions(&x))
+        .filter(|x| conditions(&x))
         .count();
     return count as i32;
 }
 
+fn a() -> i32 {
+    return scan_reports(reactor_conditions)
+}
+
 
 fn b() -> i32 {
-    2
+    return scan_reports(relaxed_conditions)
 }
 
 
