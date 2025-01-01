665171d602d1ea10ec18b8db3136796547c5a5e2
n0rrman <mail@henriknorrman.com>
2025-01-01 12:41:25 +0000

    day 10 completed

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -44,6 +44,44 @@ fn rec_calc_trails(x: usize, y: usize, val: i32, grid: &Vec<Vec<char>>) -> HashS
     return set;
 }
 
+fn calc_distinct_trails(idx: usize, grid: &Vec<Vec<char>>) -> i32 {
+    let width = grid[0].len();
+    let x = idx % width;
+    let y = idx / width;
+
+    rec_calc_distinct_trails(x, y, 0, &grid).len() as i32
+}
+
+fn rec_calc_distinct_trails(
+    x: usize,
+    y: usize,
+    val: i32,
+    grid: &Vec<Vec<char>>,
+) -> Vec<(usize, usize)> {
+    let mut vec = vec![];
+
+    if val == 9 && grid[y][x] as i32 - 48 == 9 {
+        vec.push((x, y));
+        return vec;
+    }
+
+    let new_val = val + 1;
+    if x > 0 && grid[y][x - 1] as i32 - 48 == new_val {
+        vec.extend(rec_calc_distinct_trails(x - 1, y, new_val, &grid));
+    }
+    if x < grid[0].len() - 1 && grid[y][x + 1] as i32 - 48 == new_val {
+        vec.extend(rec_calc_distinct_trails(x + 1, y, new_val, &grid));
+    }
+    if y > 0 && grid[y - 1][x] as i32 - 48 == new_val {
+        vec.extend(rec_calc_distinct_trails(x, y - 1, new_val, &grid));
+    }
+    if y < grid.len() - 1 && grid[y + 1][x] as i32 - 48 == new_val {
+        vec.extend(rec_calc_distinct_trails(x, y + 1, new_val, &grid));
+    }
+
+    return vec;
+}
+
 fn a(data: &String, grid: &Vec<Vec<char>>) -> i32 {
     data.replace("\n", "")
         .chars()
@@ -55,8 +93,15 @@ fn a(data: &String, grid: &Vec<Vec<char>>) -> i32 {
         .sum()
 }
 
-fn b(_data: &String, _grid: &Vec<Vec<char>>) -> i32 {
-    0
+fn b(data: &String, grid: &Vec<Vec<char>>) -> i32 {
+    data.replace("\n", "")
+        .chars()
+        .enumerate()
+        .map(|(i, c)| match c {
+            '0' => calc_distinct_trails(i, &grid),
+            _ => 0,
+        })
+        .sum()
 }
 
 fn main() {
