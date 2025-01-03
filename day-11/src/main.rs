cc0968b9350119bc60d1f921666867b516e669e1
n0rrman <mail@henriknorrman.com>
2025-01-03 23:21:05 +0000

    day 11 part 1 done

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-fn read_data(file_name: &str) -> Vec<i32> {
+fn read_data(file_name: &str) -> Vec<i64> {
     fs::read_to_string(file_name)
         .expect("failed to read input file")
         .split_whitespace()
@@ -8,26 +8,36 @@ fn read_data(file_name: &str) -> Vec<i32> {
         .collect()
 }
 
-fn a(data: &Vec<i32>) -> i32 {
-    let processed: Vec<i32> = data
-        .iter()
-        .flat_map(|x| {
-            let x_string = x.to_string();
-            match x_string {
-            "0" => vec![1],
-            x_string.len() % 2 == 0 => vec![1, 7],
-            _ => vec![x * 2024],
-        }})
-        .collect();
-    println!("{:#?}", processed);
-    processed.len() as i32
+fn blink(num: &i64) -> Vec<i64> {
+    if num == &0 {
+        return vec![1];
+    }
+
+    let num_str = num.to_string();
+
+    if num_str.len() % 2 == 0 {
+        return vec![
+            num_str[0..num_str.len() / 2].parse().unwrap(),
+            num_str[num_str.len() / 2..].parse().unwrap(),
+        ];
+    } else {
+        return vec![num * 2024];
+    }
 }
 
-fn b(_data: &Vec<i32>) -> i32 {
+fn a(data: &Vec<i64>) -> i64 {
+    let mut processed: Vec<i64> = data.clone();
+    for _ in 0..25 {
+        processed = processed.iter().flat_map(|x| blink(x)).collect();
+    }
+    processed.len() as i64
+}
+
+fn b(_data: &Vec<i64>) -> i64 {
     2
 }
 
 fn main() {
-    let data = read_data("test_data.txt");
+    let data = read_data("data.txt");
     print!("Part one: {}\nPart two: {}\n", a(&data), b(&data));
 }
