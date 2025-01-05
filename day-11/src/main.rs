ecc73fad697a7fe4882ff8c17f7ee9f8cea049d6
n0rrman <mail@henriknorrman.com>
2025-01-05 20:32:13 +0100

    day 11 completed

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 
 fn read_data(file_name: &str) -> Vec<i64> {
@@ -25,6 +26,31 @@ fn blink(num: &i64) -> Vec<i64> {
     }
 }
 
+fn blink_counter(map: HashMap<i64, i64>) -> HashMap<i64, i64> {
+    let mut value_map: HashMap<i64, i64> = HashMap::new();
+
+    for (k, v) in map {
+        if k == 0 {
+            map_edit(&mut value_map, 1, v);
+            continue;
+        }
+
+        let num_str = k.to_string();
+        if num_str.len() % 2 == 0 {
+            let a = num_str[0..num_str.len() / 2].parse().unwrap();
+            let b = num_str[num_str.len() / 2..].parse().unwrap();
+            map_edit(&mut value_map, a, v);
+            map_edit(&mut value_map, b, v);
+            continue;
+        } else {
+            let new_key = k * 2024;
+            map_edit(&mut value_map, new_key, v);
+            continue;
+        }
+    }
+    value_map
+}
+
 fn a(data: &Vec<i64>) -> i64 {
     let mut processed: Vec<i64> = data.clone();
     for _ in 0..25 {
@@ -33,11 +59,33 @@ fn a(data: &Vec<i64>) -> i64 {
     processed.len() as i64
 }
 
-fn b(_data: &Vec<i64>) -> i64 {
-    2
+fn b(data: Vec<i64>) -> i64 {
+    let mut value_map: HashMap<i64, i64> = HashMap::new();
+
+    // Add values from list
+    for val in data {
+        map_edit(&mut value_map, val, 1);
+    }
+
+    // 75 blinks
+    for _ in 0..75 {
+        value_map = blink_counter(value_map);
+    }
+
+    // Return "len"
+    value_map.iter().map(|(_, v)| v).sum()
+}
+
+fn map_edit(map: &mut HashMap<i64, i64>, key: i64, val: i64) {
+    if map.contains_key(&key) {
+        let old_val = map.get(&key).unwrap();
+        map.insert(key, old_val + val);
+    } else {
+        map.insert(key, val);
+    }
 }
 
 fn main() {
     let data = read_data("data.txt");
-    print!("Part one: {}\nPart two: {}\n", a(&data), b(&data));
+    print!("Part one: {}\nPart two: {}\n", a(&data), b(data));
 }
